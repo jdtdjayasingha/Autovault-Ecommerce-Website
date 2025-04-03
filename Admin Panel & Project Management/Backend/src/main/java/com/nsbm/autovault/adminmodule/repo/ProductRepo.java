8da9aa88f7b5c5fac411839d316745878d636f93
jdtdjayasingha <153268657+jdtdjayasingha@users.noreply.github.com>
2025-04-03 07:58:50 +0530

    Deleted the some file

--- Admin Panel & Project Management/Backend/src/main/java/com/nsbm/autovault/adminmodule/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nsbm.autovault.adminmodule.repo;
-
-import com.nsbm.autovault.adminmodule.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product,Integer> {
-}
