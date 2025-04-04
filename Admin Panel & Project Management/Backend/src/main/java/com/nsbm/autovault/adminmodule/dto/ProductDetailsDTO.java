2a3b89f3750b328ecc9258c095c46acea56db64b
jdtdjayasingha <153268657+jdtdjayasingha@users.noreply.github.com>
2025-04-04 23:35:07 +0530

    added comment

--- Admin Panel & Project Management/Backend/src/main/java/com/nsbm/autovault/adminmodule/dto/ProductDetailsDTO.java
+++ Admin Panel & Project Management/Backend/src/main/java/com/nsbm/autovault/adminmodule/dto/ProductDetailsDTO.java
@@ -2,40 +2,39 @@ package com.nsbm.autovault.adminmodule.dto;
 
 public class ProductDetailsDTO {
 
-    private String name;
-    private byte[] imageData;
-    private double price; // Add price field
-    private String description; // Add description field
-
-    // Getters and Setters
+    private String name; // The name of the product
+    private byte[] imageData; // The image of the product stored as a byte array
+    private double price; // The price of the product
+    private String description; // A short description of the product
+    // Getter method for the product's name
     public String getName() {
         return name;
     }
-
+    // Setter method to set the product's name
     public void setName(String name) {
         this.name = name;
     }
-
+    // Getter method for the product's image data
     public byte[] getImageData() {
         return imageData;
     }
-
+    // Setter method to set the product's image data
     public void setImageData(byte[] imageData) {
         this.imageData = imageData;
     }
-
+    // Getter method for the product's price
     public double getPrice() {
         return price;
     }
-
+    // Setter method to set the product's price
     public void setPrice(double price) {
         this.price = price;
     }
-
+    // Getter method for the product's description
     public String getDescription() {
         return description;
     }
-
+    // Setter method to set the product's description
     public void setDescription(String description) {
         this.description = description;
     }
--- Admin Panel & Project Management/Backend/src/main/java/com/nsbm/autovault/adminmodule/model/ProductDetails.java
+++ Admin Panel & Project Management/Backend/src/main/java/com/nsbm/autovault/adminmodule/model/ProductDetails.java
@@ -2,57 +2,61 @@ package com.nsbm.autovault.adminmodule.model;
 
 import jakarta.persistence.*;
 
+// This marks the class as an entity for the database
 @Entity
 public class ProductDetails {
 
+    // This is the primary key for the product, it will be auto-generated
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    // The name of the product
     private String name;
-    private double price; // Price field
-    private String description; // Description field
-
+    // The price of the product
+    private double price;
+    // A short description of the product
+    private String description;
+    // This will store the image of the product in binary format
     @Lob
     private byte[] imageData;
-
-    // Getters and Setters
+    // Getter and setter methods for the fields
+    // Get the id of the product
     public Long getId() {
         return id;
     }
-
+    // Set the id of the product
     public void setId(Long id) {
         this.id = id;
     }
-
+    // Get the name of the product
     public String getName() {
         return name;
     }
-
+    // Set the name of the product
     public void setName(String name) {
         this.name = name;
     }
-
+    // Get the price of the product
     public double getPrice() {
         return price;
     }
-
+    // Set the price of the product
     public void setPrice(double price) {
         this.price = price;
     }
-
+    // Get the description of the product
     public String getDescription() {
         return description;
     }
-
+    // Set the description of the product
     public void setDescription(String description) {
         this.description = description;
     }
-
+    // Get the image data (the product image stored in binary)
     public byte[] getImageData() {
         return imageData;
     }
-
+    // Set the image data (store the product image in binary format)
     public void setImageData(byte[] imageData) {
         this.imageData = imageData;
     }
